81dec7023d1c1dd96317da25806ecdf08d539320
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-03-17 20:53:09 +0100

    novekvo, csokkeno kesz, sakktabla hatra van

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,7 +1,10 @@
 import { tablazatMegjelenit } from "./tarolokatMegjelenit.js";
 
 export function rendezesEsemenykezelok(lista, szuloElem) {
-    let nevNo = document.getElementById("novekvo");
+    let nevNo = document.getElementById("nevNo");
+    let nevCsokk = document.getElementById("nevCsokk");
+    let gyozNo = document.getElementById("gyozNo");
+    let gyozCsokk = document.getElementById("gyozCsokk");
 
     nevNo.addEventListener("click", function () {
         console.log(this.id);
@@ -10,4 +13,32 @@ export function rendezesEsemenykezelok(lista, szuloElem) {
         });
         tablazatMegjelenit(lista, szuloElem)
     })
+
+    nevCsokk.addEventListener("click", function () {
+        console.log(this.id);
+        lista.sort(function (a, b) {
+            return a.nev < b.nev ? +1 : -1;
+        });
+        tablazatMegjelenit(lista, szuloElem)
+    })
+
+    gyozNo.addEventListener("click", function () {
+        console.log(this.id);
+        lista.sort(function (a, b) {
+            return a.gyozelmek_szama - b.gyozelmek_szama;
+        });
+        tablazatMegjelenit(lista, szuloElem)
+    })
+
+    gyozCsokk.addEventListener("click", function () {
+        console.log(this.id);
+        lista.sort(function (a, b) {
+            return b.gyozelmek_szama - a.gyozelmek_szama;
+        });
+        tablazatMegjelenit(lista, szuloElem)
+    })
+}
+
+export function sakkEllenorzo(pozicio1, pozicio2) {
+    
 }
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { ADATLISTA } from "./adat.js";
 import { tablazatMegjelenit } from "./tarolokatMegjelenit.js";
+import { rendezesEsemenykezelok } from "./fuggvenyek.js";
 
 tablazatMegjelenit(ADATLISTA);
+rendezesEsemenykezelok(ADATLISTA, document.getElementById("tablaadat"));
